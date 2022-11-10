cef585e5766be53bb0e17df87f8fbce7f354f5bb
RafaFil <filardisafa@gmail.com>
2022-11-10 15:08:16 -0300

    obtener permisos

--- backend/routes/permisos.js
+++ backend/routes/permisos.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 router.get('/', async function(req, res, next) {
     const permisos = await permisosBL.obtenerTodosLosPermisos();
+    if (permisos){
+        res.status(404).send("not found");
+    }
     res.status(200).send(permisos);
 });
 
