1f18413fc1c207e7ddb2d9a9eabeea28f5090333
Néstor Martínez <nestor.marvi@gmail.com>
2022-11-16 15:51:39 -0300

    refactor login to use an ID.

--- frontend/src/app/services/register.service.ts
+++ frontend/src/app/services/register.service.ts
@@ -9,11 +9,51 @@ import { IUser } from '../interfaces/iUser';
 })
 export class RegisterService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  registerUser(user : IUser) : Observable<boolean>{
+  registerUser(user: IUser): Observable<boolean> {
     return this.http.post<boolean>(url, user);
   }
+
+  ValidateID(id: string | null): boolean {
+    if(id == null){
+      return false;
+    }
+
+    const sevenDigitID = [1, 2, 3, 4, 7, 6];
+    const eightDigitID = [8, 1, 2, 3, 4, 7, 6]
+    const charID = id.split('');
+    let sum = 0;
+
+    if (charID.length == 7) {
+      for (let index = 0; index <= charID.length - 2; index++) {
+        let number = Number.parseInt(charID[index]);
+        sum += number * sevenDigitID[index];
+      };
+      const lastDigit = sum % 10;
+      if (lastDigit != Number.parseInt(charID[charID.length - 1])) {
+        return false;
+      }
+      return true;
+    }
+
+    else if (charID.length == 8) {
+      for (let index = 0; index <= charID.length - 2; index++) {
+        let number = Number.parseInt(charID[index]);
+        sum += number * eightDigitID[index];
+      };
+      const lastDigit = sum % 10;
+      if (lastDigit != Number.parseInt(charID[charID.length - 1])) {
+        return false;
+      }
+      return true;
+    }
+
+    else {
+      return false
+    }
+  };
+
 }
 
-const url = environment.backend_url+'/register';
+const url = environment.backend_url + '/register';
