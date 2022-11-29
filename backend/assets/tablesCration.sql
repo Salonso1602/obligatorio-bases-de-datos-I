ce57bb17234bb39e14a3e2d666cfd228490f5e33
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2022-11-29 01:41:41 -0300

    added primary keys constarints to creations

--- backend/assets/tablesCration.sql
+++ backend/assets/tablesCration.sql
@@ -22,9 +22,9 @@ CREATE TABLE APLICATIVOS_MENU(
 
 CREATE TABLE ROLES_APLICATIVOS(
     app_id int,
-    rol_id int not null auto_increment PRIMARY KEY,
+    rol_id int not null auto_increment,
     descripcion_rol varchar(255),
-
+    PRIMARY KEY (app_id, rol_id)
     FOREIGN KEY (app_id) references APLICATIVOS(app_id)
 );
 
@@ -32,23 +32,19 @@ CREATE TABLE ROLES_APLICATIVOS_MENU(
     app_id int,
     rol_id int,
     menu_id int,
-
+    PRIMARY KEY (app_id, rol_id, menu_id),
     FOREIGN KEY (app_id) references APLICATIVOS(app_id),
     FOREIGN KEY (rol_id) references ROLES_APLICATIVOS(rol_id),
-    FOREIGN KEY (menu_id) references APLICATIVOS_MENU(menu_id)
-
-);
+    FOREIGN KEY (menu_id) references APLICATIVOS_MENU(menu_id));
 
 CREATE TABLE ROLES_NEGOCIO_APLICATIVOS(
     rol_neg_id int,
     app_id int,
     rol_id int,
-
+    PRIMARY KEY (rol_neg_id, app_id, rol_id),
     FOREIGN KEY (app_id) references APLICATIVOS(app_id),
     FOREIGN KEY (rol_id) references ROLES_APLICATIVOS(rol_id),
-    FOREIGN KEY (rol_neg_id) references ROLES_NEGOCIO(rol_neg_id)
-
-);
+    FOREIGN KEY (rol_neg_id) references ROLES_NEGOCIO(rol_neg_id));
 
 CREATE TABLE PERSONAS(
     user_id varchar(9) not null PRIMARY KEY,
@@ -64,7 +60,7 @@ CREATE TABLE PERSONAS_PREGUNTAS(
     user_id varchar(9),
     preg_id int,
     respuesta varchar(100),
-
+    PRIMARY KEY (user_id, preg_id),
     FOREIGN KEY (user_id) references PERSONAS(user_id),
     FOREIGN KEY (preg_id) references PREGUNTAS(preg_id)
 );
@@ -76,7 +72,7 @@ CREATE TABLE PERMISOS(
     fecha_solicitud date not null,
     fecha_autorizacion date not null,
     estado varchar(50),
-
+    PRIMARY KEY (user_id, app_id, rol_neg_id),
     FOREIGN KEY (user_id) references PERSONAS(user_id),
     FOREIGN KEY (app_id) references APLICATIVOS(app_id),
     FOREIGN KEY (rol_neg_id) references ROLES_NEGOCIO(rol_neg_id)
